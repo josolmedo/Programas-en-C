55ba204dbeaf8e07c75da18bcf68bb646a5959c9
josolmedo <116040147+josolmedo@users.noreply.github.com>
2023-05-12 16:21:11 -0600

    Add files via upload

---
+++ Ciclo_For.c
@@ -0,0 +1,14 @@
+#include <stdio.h> //Biblioteca de funciones báiscas en C
+
+int main() //Función principal
+{ //Abre función principal
+int i; // Declaro mi variable entera de contador 
+    for(i=1; i<=5; i++) //Ciclo For
+    //Este ciclo va de 1 a 5 e imprime los números
+    {//Abro ciclo For
+             printf("%d, ", i); //Imprimo números
+             
+             }//Cierro ciclo For
+    return 0; //Regresa 0 a la función principal
+    
+    }//Ciera función principal
